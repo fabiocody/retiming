5654e762d524603d4315402c56fa7ffc772be50b
Fabio Codiglioni <fabiocody@icloud.com>
2020-06-21 12:23:42 +0200

    Add correlator graphs generation and loading

--- gutils.py
+++ gutils.py
@@ -2,6 +2,7 @@
 
 
 import networkx as nx
+from networkx.drawing.nx_pydot import write_dot, read_dot
 import matplotlib.pyplot as plt
 
 
@@ -16,3 +17,68 @@ def draw_graph(g, weights=False):
 
 def add_weighted_node(g, n, w):
     g.add_node(n, weight=w)
+
+
+def gen_correlator1():
+    g = nx.DiGraph()
+    add_weighted_node(g, 'h', 0)
+    add_weighted_node(g, 'd1', 3)
+    add_weighted_node(g, 'd2', 3)
+    add_weighted_node(g, 'd3', 3)
+    add_weighted_node(g, 'd4', 3)
+    add_weighted_node(g, 'p1', 7)
+    add_weighted_node(g, 'p2', 7)
+    add_weighted_node(g, 'p3', 7)
+    g.add_weighted_edges_from([
+        ('h', 'd1', 1),
+        ('d1', 'd2', 1),
+        ('d1', 'p1', 0),
+        ('d2', 'd3', 1),
+        ('d2', 'p2', 0),
+        ('d3', 'd4', 1),
+        ('d3', 'p3', 0),
+        ('d4', 'p3', 0),
+        ('p3', 'p2', 0),
+        ('p2', 'p1', 0),
+        ('p1', 'h', 0)
+    ])
+    write_dot(g, 'graphs/correlator1.dot')
+    return g
+
+
+def gen_correlator2():
+    g = nx.DiGraph()
+    add_weighted_node(g, 'h', 0)
+    add_weighted_node(g, 'd1', 3)
+    add_weighted_node(g, 'd2', 3)
+    add_weighted_node(g, 'd3', 3)
+    add_weighted_node(g, 'd4', 3)
+    add_weighted_node(g, 'p1', 7)
+    add_weighted_node(g, 'p2', 7)
+    add_weighted_node(g, 'p3', 7)
+    g.add_weighted_edges_from([
+        ('h', 'd1', 1),
+        ('d1', 'd2', 1),
+        ('d1', 'p1', 0),
+        ('d2', 'd3', 0),
+        ('d2', 'p2', 0),
+        ('d3', 'd4', 1),
+        ('d3', 'p3', 0),
+        ('d4', 'p3', 0),
+        ('p3', 'p2', 1),
+        ('p2', 'p1', 0),
+        ('p1', 'h', 0)
+    ])
+    write_dot(g, 'graphs/correlator2.dot')
+    return g
+
+
+def load_graph(path):
+    g = nx.DiGraph(read_dot(path))
+    nodes = g.nodes(data=True)
+    for e in g.edges:
+        for v in e:
+            v = nodes[v]
+            v['weight'] = int(v['weight'])
+        g.edges[e]['weight'] = int(g.edges[e]['weight'])
+    return g
--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
 
 import networkx as nx
-import matplotlib.pyplot as plt
-
 import gutils
 
 
 if __name__ == '__main__':
-    g = nx.DiGraph()
-    gutils.add_weighted_node(g, 1, 2)
-    gutils.add_weighted_node(g, 2, 1)
-    gutils.add_weighted_node(g, 3, 1.5)
-    gutils.add_weighted_node(g, 42, 24)
-    g.add_weighted_edges_from([(1, 2, 0.5), (2, 3, 0.75), (3, 1, 0), (1, 42, 2)])
+    g = gutils.load_graph('graphs/correlator1.dot')
     gutils.draw_graph(g)
     gutils.draw_graph(g, weights=True)
+    print(nx.dijkstra_path_length(g, 'h', 'd4'), '-', nx.dijkstra_path(g, 'h', 'd4'))
